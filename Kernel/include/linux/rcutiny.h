46e6b89d0ea2d7a6e26faaaf6a2f4b517b6acea2
KalimochoAz <calimochoazucarado@gmail.com>
2011-10-01 07:55:20 +0200

    rcu: move TINY_RCU from softirq to kthread
    If RCU priority boosting is to be meaningful, callback invocation must
    be boosted in addition to preempted RCU readers.  Otherwise, in presence
    of CPU real-time threads, the grace period ends, but the callbacks don't
    get invoked.  If the callbacks don't get invoked, the associated memory
    doesn't get freed, so the system is still subject to OOM.
    
    But it is not reasonable to priority-boost RCU_SOFTIRQ, so this commit
    moves the callback invocations to a kthread, which can be boosted easily.
    
    Signed-off-by: Paul E. McKenney <paul.mckenney@linaro.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    
    Conflicts:
    
    	include/linux/rcupdate.h
    	include/linux/rcutiny.h
    	include/linux/rcutree.h

--- Kernel/include/linux/rcutiny.h
+++ Kernel/include/linux/rcutiny.h
@@ -43,6 +43,9 @@ extern void call_rcu_sched(struct rcu_head *head,
 			   void (*func)(struct rcu_head *rcu));
 
 #define rcu_init_sched()	do { } while (0)
+static inline void rcu_init(void)
+{
+}
 
 extern void synchronize_sched(void);
 
@@ -172,16 +175,12 @@ static inline void rcu_sched_force_quiescent_state(void)
 }
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
-
 extern int rcu_scheduler_active __read_mostly;
 extern void rcu_scheduler_starting(void);
-
 #else /* #ifdef CONFIG_DEBUG_LOCK_ALLOC */
-
 static inline void rcu_scheduler_starting(void)
 {
 }
-
 #endif /* #else #ifdef CONFIG_DEBUG_LOCK_ALLOC */
 
 #endif /* __LINUX_RCUTINY_H */
