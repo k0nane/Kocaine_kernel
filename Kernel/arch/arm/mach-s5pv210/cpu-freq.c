6741497fbca78345ee7dd8be5074a098ce515614
k0nane <k0@k0nane.info>
2012-01-03 15:49:29 -0600

    Ensure cpu-freq.c is stock

--- Kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ Kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -762,37 +762,25 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
                 g_dvfslockval[i] = MAX_PERF_LEVEL;
 #endif
 
-	cpufreq_frequency_table_cpuinfo(policy, freq_table);
-	/* set default min and max policies to safe speeds */
-	policy->max = 1000000;
-	policy->min = 100000;
-	return 0;
+	return cpufreq_frequency_table_cpuinfo(policy, freq_table);
 }
 
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
-	static int max, min;
 	int ret;
-
-	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
 
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
-		max = policy->max;
-		min = policy->min;
-		policy->max = policy->min = SLEEP_FREQ;
-		ret = cpufreq_driver_target(policy, SLEEP_FREQ,
+		ret = cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
 				DISABLE_FURTHER_CPUFREQ);
 		if (ret < 0)
 			return NOTIFY_BAD;
 		return NOTIFY_OK;
 	case PM_POST_RESTORE:
 	case PM_POST_SUSPEND:
-		cpufreq_driver_target(policy, SLEEP_FREQ,
+		cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
 				ENABLE_FURTHER_CPUFREQ);
-		policy->max = max;
-		policy->min = min;
 		return NOTIFY_OK;
 	}
 	return NOTIFY_DONE;
